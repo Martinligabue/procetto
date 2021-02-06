b47c2ce83d78386c8e8505640a44d7779d4e198c
Martin <martinligabue@gmail.com>
2021-02-06 11:40:11 +0100

    non ne ho idea

---
+++ build/CMakeFiles/procetto.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for procetto.
+# This may be replaced when dependencies are built.
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/aleph/github/procetto/procetto
+# Build directory: /home/aleph/github/procetto/procetto/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
